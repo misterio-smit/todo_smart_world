c40560e18255966b9e693ebbb2ef5ad2f9e81172
Misterrio <misterio@mail.ru>
2020-10-21 19:45:20 +0700

    Подключил liquibase

--- src/main/java/com/todo/smart/worldssp/todo/domain/EntityTodo.java
+++ src/main/java/com/todo/smart/worldssp/todo/domain/EntityTodo.java
@@ -7,21 +7,42 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table
 @Data
+@Table(name = "todo_table")
+
 public class EntityTodo {
+
+    public EntityTodo() {
+    }
+
+    public EntityTodo(Long id, String shortDescription, String name, LocalDateTime creationDate, LocalDateTime updateDate, boolean done, Short priority) {
+        this.id = id;
+        this.shortDescription = shortDescription;
+        this.name = name;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+        this.done = done;
+        this.priority = priority;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String shortDescription;
+
+    @Column(nullable = false)
     private String name;
 
     @Column(updatable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime creationDate;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateDate;
+
     private boolean done = false;
+
     private Short priority;
 
 
--- src/main/java/com/todo/smart/worldssp/todo/domain/ListEntityTodo.java
+++ src/main/java/com/todo/smart/worldssp/todo/domain/ListEntityTodo.java
@@ -5,19 +5,30 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
-@Table
+@Table(name = "todo_list_table")
 @Data
 
 public class ListEntityTodo {
 
+    public ListEntityTodo() {
+    }
+
+    public ListEntityTodo(Long id, String name, LocalDateTime listCreationDate, LocalDateTime listUpdateDate) {
+        this.id = id;
+        this.name = name;
+        this.listCreationDate = listCreationDate;
+        this.listUpdateDate = listUpdateDate;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(nullable = false)
     private String name;
-    private List<EntityTodo> listEntitys;
+    //@ManyToOne
+    //private List<EntityTodo> listEntitys;
 
     @Column(updatable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
